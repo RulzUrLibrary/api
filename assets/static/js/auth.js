5098d9a4bb451056ba46445bf2eb7040f20f25df
Paul Bismuth <paul.bismuth.f@gmail.com>
2017-10-26 17:17:47 +0200

    Remove annoying popup

--- assets/static/js/auth.js
+++ assets/static/js/auth.js
@@ -1,30 +1,28 @@
-function onSignIn(googleUser) {
-	var form = document.getElementById('login')
-  var email = document.getElementById('email');
-  var token = document.getElementById('token');
-
-  token.value = googleUser.getAuthResponse().id_token;
-  email.value = googleUser.getBasicProfile().getEmail();
-  form.submit();
-}
-
-
 function attachSignin(element) {
-	console.log(element.id);
-  auth2.attachClickHandler(element, {}, onSignIn, function(error) {
-    alert(JSON.stringify(error, undefined, 2));
+  auth2.attachClickHandler(element, {}, function(googleUser) {
+    var form = document.getElementById('login')
+    var email = document.getElementById('email');
+    var token = document.getElementById('token');
+
+    token.value = googleUser.getAuthResponse().id_token;
+    email.value = googleUser.getBasicProfile().getEmail();
+
+    error.textContent = '';
+    form.submit();
+  }, function(error) {
+    console.error(error);
   });
 }
 
 var startApp = function() {
-	gapi.load('auth2', function(){
-		// Retrieve the singleton for the GoogleAuth library and set up the client.
-		auth2 = gapi.auth2.init({
-			client_id: '420546501001-v35bges8923km4s9r9p3tet8m42ibj5m.apps.googleusercontent.com',
-			cookiepolicy: 'single_host_origin',
-			// Request scopes in addition to 'profile' and 'email'
-			//scope: 'additional_scope'
-		});
-		attachSignin(document.getElementById('google-signin'));
-	});
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: '420546501001-v35bges8923km4s9r9p3tet8m42ibj5m.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      //scope: 'additional_scope'
+    });
+    attachSignin(document.getElementById('google-signin'));
+  });
 };
